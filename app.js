adaf30e4047cebddd1c79ad0c088df1d2a0d039c
Janto K <JantoK@foxmail.com>
2023-05-11 17:50:01 +0800

    添加申请单添加页面基础样式

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use('/users', usersRouter);
 app.get('/', (req, res) => res.redirect('/home'))
 app.get('/home', (req, res) => res.sendFile(path.join(viewsDir, 'index.html')))
 app.get('/checkIn', (req, res) => res.sendFile(path.join(viewsDir, 'checkIn.html')))
+app.get('/add', (req, res) => res.sendFile(path.join(viewsDir, 'add.html')))
 
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
